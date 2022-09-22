de66e8c0201817f9d7d72d2ca0bd0b42e52d2374
LuoYan111 <2621692424@qq.com>
2022-09-22 18:59:46 +0800

    第二次提交

--- ApiTest/urls.py
+++ ApiTest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Myapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/',welcome),
 ]
